6e1ea58060134367e3de13a4268fc3bcc5d91f09
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-18 23:49:46 +0500

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -7,7 +7,8 @@ text = """Python is a high-level, general-purpose programming language. Its desi
 def test_input():
     at = AppTest.from_file('app.py', default_timeout=120)
     at.run()
-    at.text_area[0].input(text).run()
-    at.button[0].click().run()
-    assert len(at.text_area(key='sum_area').value) > 0
+    at.text_area(key='t_area').input(text).run()
+    #at.text_area[0].input(text).run()
+    #at.button[0].click().run()
+    assert len(at.text_area(key='t_area').value) > 0
     assert len(at.exception) == 0
