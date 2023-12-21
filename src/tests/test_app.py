c8c0875aed71bcbb0b5dbfcc5e1beb6c517bbbd8
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 17:43:23 +0500

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -6,9 +6,10 @@ text = """Python is a high-level, general-purpose programming language. Its desi
 
 def test_input():
     at = AppTest.from_file('app.py', default_timeout=120)
+    at.secrets['API_TOKEN'] = os.getenv('API_TOKEN')
     at.run()
     at.text_area(key='t_area').input(text).run()
-    #at.text_area[0].input(text).run()
-    #at.button[0].click().run()
+    at.text_area[0].input(text).run()
+    at.button[0].click().run()
     assert len(at.text_area(key='t_area').value) > 0
     assert len(at.exception) == 0
